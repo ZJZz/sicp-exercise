418ef7f51cdb0756af34c547f48301953ae3f2b5
ZJZz <freeseabear@yahoo.com>
2021-05-25 20:30:28 +0800

    add some code for 1.7

--- chapter_1.rkt
+++ chapter_1.rkt
@@ -16,4 +16,27 @@
   (sum_of_square (max x y) (max y z))
 )
 
-(sum_of_two_largest_square 2 3 4)
+;(sum_of_two_largest_square 2 3 4)
+
+; 1.7
+
+(define (abs x)
+  (if (< x 0) (- x)
+      x)
+)
+
+(define (good_enough? guess x)
+  (< (abs (- (square guess) x)) 0.001)
+)
+
+(good_enough? 55000.00045 1000000000)
+
+(define (average x y)
+  (/ (+ x y) 2)
+)
+
+(define (improve guess x)
+  (average guess (/ x guess) )
+)
+
+(improve 9999.9999 1000000000)
