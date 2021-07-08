480063f52f79166c58b1983549b7e31eb9dd9be4
ZJZz <freeseabear@yahoo.com>
2021-07-08 13:14:50 +0800

    reformat text code for better reading

--- chapter_1.rkt
+++ chapter_1.rkt
@@ -646,3 +646,32 @@
 ; after validate in the IDE
 ; (f, f) is same error with (2, 2) 
 
+(define (f x y)
+  (let ((a (+ 1 (* x y)))
+        (b (- 1 y))
+       )
+       (+ (* x (square a)) (* y b) (* a b))
+  )
+)
+
+(define (search f neg-point pos-point)
+  (let ((midpoint (average neg-point pos-point)))
+       (if (close-enough? neg-point pos-point) midpoint
+          (let ((test-value (f midpoint)))
+               (cond ((positive? test-value) (search f neg-point midpoint))
+                     ((negative? test-value) (search f midpoint pos-point))
+                     (else midpoint)
+               )
+          )
+       )
+  )
+)
+
+(define (half-interval-method f a b)
+    (let ((a-value (f a)) (b-value (f b)))
+         (cond ((and (negative? a-value) (positive? b-value)) (search f a b))
+               ((and (negative? b-value) (positive? a-value)) (search f b a))
+               (else (error "Values are not of opposite sign" a b))
+         )
+    )
+)
