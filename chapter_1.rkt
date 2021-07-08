a3582efe12de13ea1811480980fc5d5cc286b8ee
ZJZz <freeseabear@yahoo.com>
2021-07-08 20:23:02 +0800

    finish 1.35

--- chapter_1.rkt
+++ chapter_1.rkt
@@ -668,10 +668,31 @@
 )
 
 (define (half-interval-method f a b)
-    (let ((a-value (f a)) (b-value (f b)))
+    (let ((a-value (f a)) 
+          (b-value (f b)))
          (cond ((and (negative? a-value) (positive? b-value)) (search f a b))
                ((and (negative? b-value) (positive? a-value)) (search f b a))
-               (else (error "Values are not of opposite sign" a b))
-         )
+               (else (error "Values are not of opposite sign" a b)))
     )
 )
+
+(define tolerance 0.00001)
+(define (fixed-point f first-guess)
+  (define (close-enough? v1 v2)
+    (< (abs (- v1 v2)) tolerance))
+  (define (try guess)
+    (let ((next (f guess)))
+         (if (close-enough? guess next) next 
+             (try next)
+         )
+    )
+  )
+  (try first-guess)
+)
+
+; 1.35
+
+(fixed-point (lambda (x) (/ (+ x 1) x)) 2)
+; output is 1.(377/610) which is close 1.61803
+
+; 1.36
