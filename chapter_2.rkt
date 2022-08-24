4c2a11db34c79a16850cbf3c5e16a7be76390392
ZJZz <freeseabear@yahoo.com>
2022-08-24 17:14:55 +0800

    start chapter 2

---
+++ chapter_2.rkt
@@ -0,0 +1,61 @@
+#lang racket
+
+(define (add-rat x y)
+  (make-rat (+ (* (numer x) (denom y))
+               (* (numer y) (denom x)))
+            (* (denom x) (denom y))))
+
+(define (sub-rat x y)
+  (make-rat (- (* (numer x) (denom y))
+               (* (numer y) (denom x)))
+            (* (denom x) (denom y))))
+
+(define (mul-rat x y)
+  (make-rat (* (numer x) (numer y))
+            (* (denom x) (denom y))))
+
+(define (div-rat x y)
+  (make-rat (* (numer x) (denom y))
+            (* (denom x) (numer y))))
+
+(define (equal-rat? x y)
+  (= (* (numer x) (denom y))
+     (* (numer y) (denom x))))
+
+
+(define (gcd a b)
+  (if (= b 0)
+      a
+      (gcd b (remainder a b))))
+
+(define (numer x) (car x))
+(define (denom x) (cdr x))
+
+(define (print-rat x)
+  (newline)
+  (display (numer x))
+  (display "/")
+  (display (denom x)))
+
+
+; 2.1
+(define (abs x)
+  (if (< x 0) (- x)
+      x)
+
+  
+)
+
+(define (make-rat n d)
+  (let ((g (gcd (abs n) (abs d))))
+    (if (> (* n d) 0) (cons (/ n g) (/ d g))
+        (cons (- (/ (abs n) g)) (/ (abs d) g))
+     )
+    )
+)
+
+(define one-half (make-rat 2 -4))
+
+(print-rat one-half)
+
+  
